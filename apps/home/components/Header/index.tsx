60447be6b959a1b8feddaf187627265c9e8dd4b6
Matias Salicru <40377128+matiassalicru@users.noreply.github.com>
2023-12-17 20:51:09 -0300

    feature: add MUI for styling

---
+++ apps/home/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from '@mui/material'
+
+export const Header = () => {
+  return (
+    <Box
+      style={{
+        display: 'flex ',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography>Home</Typography>
+    </Box>
+  )
+}
